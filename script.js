1a45e873ed68a7118edbd8e5a8018ec1faf124d9
bardach01 <abdoubardachcode@gmail.com>
2023-03-31 03:20:30 +0000

    Update-1.0

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const z = document.getElementById("equation");
+    const x = "";
+    const y = x.slice(-1);
+    if(y == "+", y == "-", y == "*", y == "/", y == "%") {
+    document.getElementById("equation").innerHTML = "hello";
+    }
+}
